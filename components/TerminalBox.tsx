c218fd4e50a8bf0886e66f75ff2b8dbb3586e1ce
TokunagaX <officialxyc@gmail.com>
2024-05-13 22:34:20 +0800

    feat: add date to window, init ref to listen  keyboard event

--- components/TerminalBox.tsx
+++ components/TerminalBox.tsx
@@ -1,20 +1,43 @@
-// import React, { useEffect, useState, useRef } from "react";
-// import renderCmd from "../utils/renderCmd";
-// import CmdUserInput from "./CmdUserInput";
-// import EnteredCmd from "./EnteredCmd";
-// import TodayDate from "./TodayDate";
-// import Help from "./commands/Help";
-// import Navbar from "./Navbar";
-// import Welcome from "./commands/Welcome";
+'use client'
+
+import React, { useEffect, useRef, useState } from "react";
 
 import Navbar from "./Navbar";
+import TodayDate from "./TodayDate";
+import Welcome from "./commands/Welcome";
 
 export default function TerminalBox() {
+	const dummyRef = useRef() as React.MutableRefObject<HTMLDivElement>;
 
+	const [enteredCmd, setEnteredCmd] = useState([
+		{
+			cmd: "",
+			Component: Welcome,
+			time: new Date().toLocaleTimeString(),
+		},
+	]);
+
+	useEffect(() => {
+		dummyRef.current.scrollIntoView({ behavior: "auto" });
+	}, [enteredCmd]);
+
+	useEffect(() => {
+		document.body.addEventListener("keydown", handleKeyEvent);
+	}, []);
+
+	const handleKeyEvent = (e: KeyboardEvent) => {
+		if (e.ctrlKey && e.key.toLocaleLowerCase() === "l") {
+			setEnteredCmd([]);
+		}
+	};
+	
 	return (
 		<div>
 			<Navbar />
 			<div className="max-w-4xl border-x-2 border-b-2 border-slate-800 rounded-b-md mx-auto text-gray-300 text-xl p-2 overflow-y-auto h-55vh bg-black bg-opacity-75 box">
+			<TodayDate />
+
+			<div ref={dummyRef}></div>
 			</div>
 		</div>
 	);
