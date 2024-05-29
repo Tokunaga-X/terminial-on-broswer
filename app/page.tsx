53025ad3728244c4b314b19c46525a01d4dfc29a
TokunagaX <officialxyc@gmail.com>
2024-05-29 22:52:00 +0800

    code format using prettier

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,6 @@ export default function Home() {
 					<div className="block sm:hidden mb-10 bg-yellow-200 py-3  text-center text-xl text-gray-700">
 						<p>For better experience please try this with pc!</p>
 						<p>Currently, This is not design for mobile view.</p>
-						{/* <a href="https://chensokheng.me" className="block p-2 underline">
-							Visit this instead
-						</a> */}
 					</div>
 					<TerminalBox />
 				</div>
